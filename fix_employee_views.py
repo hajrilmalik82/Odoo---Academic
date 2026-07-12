233f1e0d61671aa02334d8fc6476080568b38e7d
Hajril <hajrilmalik82@gmail.com>
2026-07-12 20:02:14 +0700

    p

--- fix_employee_views.py
+++
@@ -1,5 +0,0 @@
-import xml.etree.ElementTree as ET
-
-tree = ET.parse('/home/hajril/odoo-dev/academic-19/campus_employees/views/hr_employee_views.xml')
-# verify we can parse it
-print("Parsed XML successfully.")
